473ddb566b50cc445e9645a4d1b3e3617984363f
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-09 11:07:00 -0600

    Renamed the route "home".  In Home Controller renamed the method to homePage() and update the View::make() to call the appropriate view.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,9 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	public function homePage()
 	{
-		return View::make('hello');
+		return View::make('home');
 	}
 
 }
