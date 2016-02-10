fcc72ebc5fbb674048ede2a838271fb9015fe3e7
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 11:31:33 -0600

    Organizing the views.  Layouts contains the master layout

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>San Antonio Valuations</title>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/main.css">
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_ltk7t5oRK_HAoJoo4JQh9R1vz4-1mnU&amp;libraries=places&amp;sensor=false"
+	   			type="text/javascript"></script>
+		<script src="/js/main.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row">
+				<img src="/img/theAlamo.jpg" class="img-responsive" alt="Responsive image">
+			</div>
+		</div>
+		<div class="container-fluid top-box">
+			@yield('content')
+		</div>
+		<footer>
+			@include('partials.prospect_footer')
+		</footer>
+	</body>
+</html>
+
+<!--
+TODO:  Center elements inside form
+*TODO:  vertically responsive
+TODO:  Invalid address detection - not allowed to proceed
+ -->
