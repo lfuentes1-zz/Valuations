cd0a02704e1170d92ce6366245be8519e754375a
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 14:12:17 -0600

    Added logic to execute the seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,8 +10,17 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+		DB::table('specialties')->delete();
+		DB::table('prospects')->delete();
+		DB::table('agents')->delete();
+		DB::table('brokerages')->delete();
+		DB::table('brokers')->delete();
 
-		// $this->call('UserTableSeeder');
+		//the above ensures fresh data everytime
+		$this->call('BrokerTableSeeder');
+		$this->call('BrokerageTableSeeder');
+		$this->call('AgentTableSeeder');
+		// $this->call('ProspectTableSeeder');
+		$this->call('SpecialtyTableSeeder');
 	}
-
 }
