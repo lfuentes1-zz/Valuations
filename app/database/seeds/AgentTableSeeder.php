fbceaa2501dd0665b0049b3d8375ba7f896d6588
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 14:10:00 -0600

    Added content for one record

---
+++ app/database/seeds/AgentTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+class AgentTableSeeder extends Seeder 
+{     
+      public function run()
+      {
+            $agent = new Agent();
+            $agent->agent_first_name      = 'Leticia';
+            $agent->agent_last_name       = 'Fuentes';
+            $agent->agent_phone_number    = '210.213.9803';
+            $agent->agent_email           = 'letty@alamocitydreamhomes.com';
+            // $agent->agent_photo           = '';  //can be null
+            $agent->agent_password        = $_ENV['AGENT_PASS'];
+            // $agent->agent_listing_leads   = ''  //can be null
+            // $agent->agent_listing_appointments  = '' //can be null
+            // $agent->agent_listings_taken  = ''  //can be null
+            // $agent->agent_listings_sold   = ''  //can be null
+            // $agent->agent_lead_to_sold_rate= '';  //can be null
+            $agent->agent_dollar_volume   = ''; //can be null
+            $agent->brokerage_id          = Brokerage::findorFail(1)->id;           
+            $agent->broker_id             = Broker::findorFail(1)->id;           
+            $agent->save();
+      }     
+}
