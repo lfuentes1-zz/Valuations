cec3332518d3ba444f20c89c03c20160f96cabd9
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 14:09:17 -0600

    Manually changed the timestamp on the file so that the migration would appear after the broker migration

---
+++ app/database/migrations/2016_02_08_215840_create_brokerages_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrokeragesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('brokerages', function($table)
+		{
+            $table->increments('id');
+            $table->string('brokerage_name', 250);
+			$table->string('brokerage_address', 250);
+			$table->string('brokerage_unit_suite', 10)->nullable();
+            $table->string('brokerage_city', 150);
+            $table->string('brokerage_state', 20);
+            $table->string('brokerage_zip', 10);
+            $table->string('brokerage_phone_number', 20);
+			$table->string('brokerage_email', 200)->unique();
+            $table->string('brokerage_logo')->nullable();
+            $table->integer('broker_id')->unsigned();
+			$table->foreign('broker_id')->references('id')->on('brokers');
+            $table->timestamps();
+
+            // $table->string('slug')->nullable();
+            // $table->rememberToken();
+            // $table->softDeletes();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('brokerages');
+	}
+
+}
