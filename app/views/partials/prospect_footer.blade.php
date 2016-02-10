26d908816bfa994e6701e99a7f484983448e287a
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 11:32:11 -0600

    Organizing the views.  This directory includes all the partials.

---
+++ app/views/partials/prospect_footer.blade.php
@@ -0,0 +1,16 @@
+<div class="row footer">
+	<div class="col-md-12 col-xs-18 col-sm-18">
+		<div class="col-md-4">
+			<a href="#" class="navbar-brand">Leticia Fuentes, Realtor® | Texas Premier Realty | 210.213.9803</a>
+		</div>
+		<div class="col-md-4">
+			<a href="#" class="navbar-brand">About Us |</a>
+			<a href="#" class="navbar-brand">Login</a>
+		</div>
+		<div class="col-md-4">
+			<a href="#" class="navbar-brand">®Copyright 2016 | Privacy Policy</a>
+		</div>
+	</div>
+</div>
+
+
