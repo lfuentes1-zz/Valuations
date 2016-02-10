b2ce144544db533941c673a19d6e6ad2c5e7a6ca
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-10 14:13:30 -0600

    Added logic to store prospect into the database

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,15 +8,35 @@ class HomeController extends BaseController {
 		return View::make('index');
 	}
 
-	public function additionalInformation(/*will need a id parameter here*/)
+	public function additionalInformation()
 	{
-		//store the prospect into the database
-		echo ('stored');
-		return View::make('additional_information');
+
+		$prospect = new Prospect();
+		$prospect->prospect_adress = ;
+	    $prospect->prospect_apt_unit = ;
+        $prospect->prospect_city = ;
+        $prospect->prospect_state = ;
+
+		if ($prospect->save()) {  //true returns true or false
+			$prospect->agent_id  = Agent::findOrFail(1)->id;  //formula to determine agent
+			$prospect->broker_id = Broker::findOrFail(1)->id;  //formula to determine agent
+			return Redirect::route('additional_information', $prospect->id);
+		} else {
+			Session::flash('errorMessage', 'Unable to locate property!');
+			return Redirect::back()->withInput();
+		}
+		// return View::make('additional_information');
 	}
 
 	public function thankYou()
 	{
+		//update the prospect into the database
+		echo ('updated');
 		return View::make('thank_you');
 	}
+
+	
 }
+
+
+// TODO:  Formula to determine which agent gets the leads or round robin
