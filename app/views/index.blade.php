bfaf9538563e70f061335df85a2b06f21e1b8759
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-11 15:54:57 -0600

    Added a @section('head') with JS to be appended to by child views

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,32 +1,38 @@
 @extends('layouts.master')
 
+@section('head')
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_ltk7t5oRK_HAoJoo4JQh9R1vz4-1mnU&amp;libraries=places"
+		type="text/javascript"></script>
+	<script src="/js/index.js"></script>
+@stop
+
 @section('content')
 
-{{ Form::open(array('action' => 'HomeController@store', 'method' => 'POST', 'id' => 'create-form')) }}	
+	{{ Form::open(array('action' => 'HomeController@store', 'method' => 'POST', 'id' => 'create-form')) }}	
 
-<div class="row">
-	<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3 jumbotron">
-		<div class="jumbotron-heading text-center">
-			<h2>What is your San Antonio home worth?</h2>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3 jumbotron">
+			<div class="jumbotron-heading text-center">
+				<h2>What is your San Antonio home worth?</h2>
+			</div>
+			<div class="jumbotron-subheading text-center">
+				<h3>Get your <span class="jumbotron-special-txt">FREE</span> Home Market Analysis Report</h3>
+			</div>
+			<form class="form-inline">
+				<div class="form-group">
+			    	{{ Form::label('prospect-address', ' ') }}
+			        {{ Form::text('prospect-address', null, ['class' => 'form-control', 'placeholder' => 'Address', 'autocomplete' => 'on']) }}
+			  	</div>
+			  	<div class="form-group">
+			  		{{ Form::label('prospect-unit-number', ' ') }}
+			        {{ Form::text('prospect-unit-number', null, ['class' => 'form-control', 'placeholder' => 'Unit Number']) }}
+			  	</div>
+			  	{{ Form::submit('Submit', ['class' => 'btn btn-success']) }}
+			</form>
 		</div>
-		<div class="jumbotron-subheading text-center">
-			<h3>Get your <span class="jumbotron-special-txt">FREE</span> Home Market Analysis Report</h3>
-		</div>
-		<form class="form-inline">
-			<div class="form-group">
-		    	{{ Form::label('prospect-address', ' ') }}
-		        {{ Form::text('prospect-address', null, ['class' => 'form-control', 'placeholder' => 'Address', 'autocomplete' => 'on']) }}
-		  	</div>
-		  	<div class="form-group">
-		  		{{ Form::label('prospect-unit-number', ' ') }}
-		        {{ Form::text('prospect-unit-number', null, ['class' => 'form-control', 'placeholder' => 'Unit Number']) }}
-		  	</div>
-		  	{{ Form::submit('Submit', ['class' => 'btn btn-success']) }}
-		</form>
 	</div>
-</div>
 
-{{ Form::close() }}	
+	{{ Form::close() }}	
 
 @stop
 
