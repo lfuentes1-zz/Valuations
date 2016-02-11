6e137b64be0f83e41e8eaf4db31b44c537884c08
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-11 15:56:20 -0600

    Added a @section('head') to be appended to to child views

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,9 +6,9 @@
 		<title>San Antonio Valuations</title>
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link rel="stylesheet" href="/css/main.css">
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_ltk7t5oRK_HAoJoo4JQh9R1vz4-1mnU&amp;libraries=places&amp;sensor=false"
-	   			type="text/javascript"></script>
-		<script src="/js/main.js"></script>
+		@section('head')
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	   	@show
 	</head>
 	<body>
 		<div class="container-fluid">
@@ -28,5 +28,8 @@
 <!--
 TODO:  Center elements inside form
 *TODO:  vertically responsive
-TODO:  Invalid address detection - not allowed to proceed
+TODO:  Invalid address detection - not allowed to proceed..
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_ltk7t5oRK_HAoJoo4JQh9R1vz4-1mnU&amp;libraries=places&amp;sensor=false"
+	   			type="text/javascript"></script>
  -->
