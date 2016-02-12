dd2ee9dcc8f60a792e3bd8967188ac47d678525c
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-11 21:53:39 -0600

    Implementation still underway.  Added the hidden fields for the lat and long

--- app/views/additional_information.blade.php
+++ app/views/additional_information.blade.php
@@ -1,44 +1,45 @@
 @extends('layouts.master')
 
+@section('head')
+	@parent
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_ltk7t5oRK_HAoJoo4JQh9R1vz4-1mnU"></script>
+	<script src="/js/additional_information.js"></script>
+@stop
+
 @section('content')
-
-{{ Form::model(array('action' => 'HomeController@thankYou', 'method' => 'PUT')) }}
-
-
-	  		{{ Form::model($group, array('action' => array('GroupsController@update', $group->id), 'method' => 'PUT')) }}
-
-
-<div class="row">
-	<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3 jumbotron">
-		<div class="jumbotron-heading text-center">
-			<h2>Additional Information</h2>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3 jumbotron">
+			<div class="">
+				{{ Form::label('latitude', 'Lat') }}
+	        	{{ Form::text('latitude', $prospect->prospect_address_lat) }}
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="jumbotron-heading text-center" id="prospect-address">
+						<h3>{{{"Please complete to receive your FREE Home Market Report"}}}
+							need a form here
+						<h2>{{{"Property Details"}}}
+						<h4>{{{$prospect->prospect_address}}}</h4>
+							need a form here
+					</div>	
+				</div>
+				<div class="col-md-8">
+					<div class="img-responsive" id="map-canvas"></div>
+				</div>
+			</div>
+			<div class="">
+				{{ Form::label('longitude', 'Lng') }}
+	       		{{ Form::text('longitude', $prospect->prospect_address_lng) }}
+		    </div>
 		</div>
-		<div class="jumbotron-subheading text-center">
-			<h3>Get your <span class="jumbotron-special-txt">FREE</span> Home Market Analysis Report</h3>
-		</div>
-		<form class="form-inline">
-			<div class="form-group">
-		    	<label for="prospect-address"></label>
-		    	<input type="text" class="form-control" id="prospect-address" placeholder="Address" autocomplete="on" size="55">
-		  	</div>
-		  	<div class="form-group">
-		    	<label for="prospect-unit-number"></label>
-		    	<input type="text" class="form-control" id="prospect-unit-number" placeholder="Unit #">
-		  	</div>
-		  	<button type="submit" class="btn btn-success">Submit</button>
-		</form>
 	</div>
-</div>
-
-{{ Form::close() }}
-
 @stop
 
 
+{{--
 
-
-{{-- 
-
-TODO:  Might need something like this:  {{ Form::model($group, array('action' => array('GroupsController@update', $group->id), 'method' => 'PUT')) }} 
+TODO:  need to fix the layout of this page
+TODO:  need the two forms
 
 --}}
+
