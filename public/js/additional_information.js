9fa3f54e35897ef89a40f422055ad99a4206e4f3
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-12 15:55:55 -0600

    Added the marker upon page load. Added the info window upon page load with the property information on it.

--- public/js/additional_information.js
+++ public/js/additional_information.js
@@ -1,24 +1,45 @@
-(function() {
+$(document).ready(function() {
 	"use strict";
 
 	//*--------uses google maps api to display the map of the property on additional_information.blade.php--------*
-	function initialize() {
-		var lat = $('#latitude').val();
-		var lng = $('#longitude').val();
+	var latitude = parseFloat($('#latitude').val());
+	var longitude = parseFloat($('#longitude').val());
+	var prospectAddress = $('#prospect-address').val();
+	var prospectAddress = prospectAddress.split(", ");
 
-	    var latlng = new google.maps.LatLng(lat, lng);
-	    // Set our map options
-	    var myOptions = {
-	        zoom: 15,
-	        center: latlng,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP	
-	    };
-	    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	}
+    // Set our map options
+    var myOptions = {
+        zoom: 18,
+        center: {
+        	lat: latitude,
+        	lng: longitude
+        },
+    };
 	// Render the map
-	google.maps.event.addDomListener(window, "load", initialize);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 
-})();
+	var markerLabel = '<div>'+		      
+				      '<h5>' + prospectAddress[0] + '</h5>' +
+				      '<h5>' + prospectAddress[1] + ', ' + prospectAddress[2] + '</h5>' +
+				      '</div>';
+					
+    //used for the info window
+	var infowindow = new google.maps.InfoWindow({
+		content: markerLabel
+	});
+	
+    //Place the marker on the map
+	var marker = new google.maps.Marker({
+		position: {lat: latitude, lng: longitude},
+		map: map,
+	});
 
-// TODO:  add the marker on the address
+	//place the marker on the window upon loading of the page
+    infowindow.open(map, marker);
+
+
+});
+
 // TODO:  figure out the zip code to the address
+// TODO:  Google Maps API has street view option
+
