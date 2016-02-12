e1126b79ba55dc2574bc4199cb67b670f4d1181d
Letty Fuentes <lettyfuentes2013@gmail.com>
2016-02-12 15:54:54 -0600

    Added the hidden fields for the lat and long and prospect address

--- app/views/additional_information.blade.php
+++ app/views/additional_information.blade.php
@@ -8,29 +8,39 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3 jumbotron">
-			<div class="">
-				{{ Form::label('latitude', 'Lat') }}
-	        	{{ Form::text('latitude', $prospect->prospect_address_lat) }}
-			</div>
-			<div class="row">
-				<div class="col-md-4">
-					<div class="jumbotron-heading text-center" id="prospect-address">
-						<h3>{{{"Please complete to receive your FREE Home Market Report"}}}
-							need a form here
-						<h2>{{{"Property Details"}}}
-						<h4>{{{$prospect->prospect_address}}}</h4>
-							need a form here
-					</div>	
+		<div class="col-md-8 col-md-offset-2 col-xs-12 col-sm-6 col-sm-offset-3">
+			<div class="jumbotron">
+				<div class="hidden">
+					{{ Form::label('latitude', 'Lat') }}
+		        	{{ Form::text('latitude', $prospect->prospect_address_lat) }}
 				</div>
-				<div class="col-md-8">
-					<div class="img-responsive" id="map-canvas"></div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="jumbotron-heading text-center">
+							<h3>{{{"Please complete to receive your FREE Home Market Report"}}}
+							{{ Form::label('latitude', 'Lat') }}
+		        			{{ Form::text('latitude', $prospect->prospect_address_lat) }}
+		        			{{ Form::label('latitude', 'Lat') }}
+		        			{{ Form::text('latitude', $prospect->prospect_address_lat) }}
+
+							<h2>{{{"Property Details"}}}
+							<h4>{{{$prospect->prospect_address}}}</h4>
+								need a form here
+						</div>	
+					</div>
+					<div class="col-md-8">
+						<div class="img-responsive" id="map-canvas"></div>
+					</div>
 				</div>
+				<div class="hidden">
+					{{ Form::label('longitude', 'Lng') }}
+		       		{{ Form::text('longitude', $prospect->prospect_address_lng) }}
+			    </div>
+			    <div class="hidden">
+					{{ Form::label('prospect-address', 'Address') }}
+		       		{{ Form::text('prospect-address', $prospect->prospect_address . ', ' . $prospect->prospect_city . ', ' . $prospect->prospect_state) }}
+			    </div>
 			</div>
-			<div class="">
-				{{ Form::label('longitude', 'Lng') }}
-	       		{{ Form::text('longitude', $prospect->prospect_address_lng) }}
-		    </div>
 		</div>
 	</div>
 @stop
@@ -40,6 +50,7 @@
 
 TODO:  need to fix the layout of this page
 TODO:  need the two forms
+
 
 --}}
 
